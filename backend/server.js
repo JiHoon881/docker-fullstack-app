9c4db827043448f8ef40a9c9c27df4c8162313cd
KJH <khjjh1445@gmail.com>
2020-12-08 10:42:33 +0900

    test

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,7 @@ app.get('/api/hi', function (req, res) {
 // DB lists 테이블에 있는 모든 데이터를 프론트 서버에 보내주기
 app.get('/api/values', function(req, res) {
     // 데이터베이스에서 모든 정보 가져오기
+    db.pool.query(`DELETE FROM lists WHERE value = ''`),
     db.pool.query('SELECT * FROM lists;',
     (err, results, fileds) => {
         if (err)
@@ -40,8 +41,7 @@ app.get('/api/values', function(req, res) {
 // 클라이언트에서 입력한 값을 데이터베이스 lists 테이블에 넣어주기
 app.post('/api/value', function (req, res, next) {
     // 데이터베이스에 값 넣어주기
-    db.pool.query(`INSERT INTO lists (value) VALUES("${req.body.value}")`, // delete from lists where value = '';
-    db.pool.query(`DELETE FROM lists WHERE value = ''`),
+    db.pool.query(`INSERT INTO lists (value) VALUES("${req.body.value}")`,
     (err, results, fileds) => {
         if (err)
             return res.status(500).send(err)
